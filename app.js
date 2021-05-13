7a1f330db726e46c0fcdedb36712763a5d42fef6
sanjay@crownstack <sanjay.barman@crownstack.com>
2021-05-13 10:43:44 +0530

    working with modules

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 console.log("heloo sanjay");
 
-const addition = require('./addition')
-console.log(addition(10,1));
+const addition = require('./tutorial')
+
+
+// console.log(addition(10,1));
+
+console.log(addition.sum(110,1));  
+console.log(addition.PI);
+console.log(new addition.SomeMathObject());
