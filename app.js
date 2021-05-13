cf4d90affcd1c6ac78182004b4a9ca04cffefda1
sanjay@crownstack <sanjay.barman@crownstack.com>
2021-05-13 13:51:07 +0530

    add eventListner

--- app.js
+++ app.js
@@ -1,10 +1,50 @@
-console.log("heloo sanjay");
+// console.log("heloo sanjay");
 
-const addition = require('./tutorial')
-
+// const addition = require('./tutorial')
 
 // console.log(addition(10,1));
 
-console.log(addition.sum(110,1));  
-console.log(addition.PI);
-console.log(new addition.SomeMathObject());
+// console.log(addition.sum(110,1));  
+// console.log(addition.PI);
+// console.log(new addition.SomeMathObject());
+
+
+
+
+const EventEmitter = require('events');
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on('tutorial', (num1 , num2) =>{            // listen
+    console.log('event has occured succesfully');
+    console.log(num1 + num2);
+});
+
+eventEmitter.emit('tutorial',10,1);     // publish
+
+
+
+class Person extends EventEmitter {
+    constructor(name) {
+        super();
+        this._name = name;
+    }
+
+    get name() {
+        return this._name;
+    }
+}
+
+
+let perdo = new Person('Pedro');
+let sanjay = new Person('sanjay');
+
+sanjay.on('name', () =>{
+    console.log('my name is '+sanjay.name);
+})
+
+perdo.on('name', () =>{
+    console.log('my name is '+perdo.name);
+});
+
+perdo.emit('name');
+sanjay.emit('name');
